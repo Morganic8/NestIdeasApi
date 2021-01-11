e688a3879fad7e417fe049b790985da955f8dbff
Morgan Atwood <atwood.morganj@gmail.com>
2021-01-11 14:46:17 -0800

    API set up

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
 import 'dotenv/config';
+import { NestFactory } from '@nestjs/core';
 import { Logger } from '@nestjs/common';
 
+import { AppModule } from './app.module';
+
 const port = process.env.PORT || 8080;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
